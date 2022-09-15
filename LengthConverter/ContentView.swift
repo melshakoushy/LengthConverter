93fa47adffd97d02275b5581e0f2dd998299fd9f
Mahmoud Elshakoushy <MElshako@its.jnj.com>
2022-09-15 14:25:15 +0200

    first commit

---
+++ LengthConverter/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LengthConverter
+//
+//  Created by Mahmoud Elshakoushy on 15/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    //MARK: - Body
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by Mahmoud Elshakoushy on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
